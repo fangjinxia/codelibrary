4a02b657f9ef18c7a4dd837a0a23af3ad101298a
fangjinxia <fangjinxia@iie.ac.cn>
2017-08-22 20:20:02 +0800

    扩展欧几里得算法
    可参照poj1061题

---
+++ codelib/gcd_extend.cpp
@@ -0,0 +1,13 @@
+//可参考poj1061
+void gcd_extend(long long a,long long b,long long &g,long long &x,long long &y)
+{
+	if (!b)
+	{
+		g = a;
+		x = 1;
+		y = 0;
+		return;
+	}
+	gcd_extend(b, a % b, g, y, x);
+	y -= a / b * x;
+}
