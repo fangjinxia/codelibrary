1ec6d69993f7e044abefcce461696196e1623bb6
fangjinxia <fangjinxia@iie.ac.cn>
2017-08-25 16:25:30 +0800

    求最大值下标

--- codelib/vector.cpp
+++ codelib/vector.cpp
@@ -2,7 +2,7 @@
  * vector基本操作
  *
  */
- vector<int> arr;
+vector<int> arr;
  
  
 ///<返回elem的最后一个可安插位置，也就是“元素值>elem”的第一个元素位置
@@ -18,5 +18,7 @@ res.assign(arr.begin()+lo, arr.begin()+hi+1);///<注意加1
 ///<复制区间[beg, end]内元素，赋值给v
 arr.assign(beg, end) //复制区间[beg, end]内元素，赋值给arr
 
-
-
+///<求nums下标low到high之间的最大值，并求出最大值下标，
+vector<int> nums;
+vector<int>::iterator iter = max_element(nums.begin() + low, nums.begin()+high+1);///<注意加1
+int idx = distance(nums.begin(), iter);///<位置靠前的放前面
