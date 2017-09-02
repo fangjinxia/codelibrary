fb98f2a028661b1261bb4d16400c8a2a38e50210
fangjinxia <fangjinxia@iie.ac.cn>
2017-09-02 16:39:42 +0800

    逆转string

---
+++ codelib/string.cpp
@@ -0,0 +1,12 @@
+﻿/*
+ * string常见操作
+ *
+ */
+
+///<逆转
+string s = "I am a student.";
+
+std:: void reverse(s.begin(), s.end());///<原地反转
+
+string s1 = "";
+s1.assign(s.rbegin(), s.rend());///<反转到s1里面
